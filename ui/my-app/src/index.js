64817b96fe283a5a30cb8880c81e5372f782fc24
siergieyk <siergieyk@gmail.com>
2020-01-20 20:38:40 -0800

    Toggle Switch
    This toggle switch controlss the "on" and "off" text.

--- ui/my-app/src/index.js
+++ ui/my-app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ToggleSwitch from './components/toggleSwitch';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ToggleSwitch />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
