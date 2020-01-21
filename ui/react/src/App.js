003d608ec76d679d1b30ec69d4ee304326fbfb67
Iryna <iryna.sherepot@gmail.com>
2020-01-20 16:53:16 -0800

    added back accidentally removed files

--- ui/react/src/App.js
+++ ui/react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TextComponent from './TextComponent'
+// renders main page together with new Text Component
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">      
+        <TextComponent/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
