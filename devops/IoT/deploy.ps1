8202c0c6af7e1764a10cc3536a5b243154f6a08a
Yozelinm <yozelin.m_206@yahoo.com>
2020-01-23 18:12:18 -0800

    Adding parameters file and ps file
    Adding parameters file and ps file inside new IoT subfolder

---
+++ devops/IoT/deploy.ps1
@@ -0,0 +1,12 @@
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+# Create the storage account.
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName "Standard_LRS"
+
+# Retrieve the context.
+$ctx = $storageAccount.Context
