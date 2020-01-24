9ab2cc248163f6bc96b92ce9bf8aa8625ba2a146
hken920 <hken920@gmail.com>
2020-01-24 15:07:36 -0800

    added base64 statement. Have not been tested yet

--- backend/base64 decode/__init__.py
+++ backend/base64 decode/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import base64
 
 import azure.functions as func
 
@@ -21,4 +22,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
              status_code=400
+        )
+
+    req_body = req.get_json()
+    base64str = req_body.get('base64String')
+    if base64Str:
+        base64decoded = base64.b64decode(base64str)
+        logging.info('Base64 Decoded: ' + base64decoded)
+        return func.HttpResponse(f"Decoded base64: {base64decoded}")
+    else:
+        return func.HttpResponse(
+            "Base64 String not found,",
+            status_code=400
         )
