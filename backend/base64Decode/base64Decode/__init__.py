112c7ac7037c824b686abae5d015bf1208dd12da
hken920 <hken920@gmail.com>
2020-01-25 15:56:39 -0800

    redid base64Decpde folder

---
+++ backend/base64Decode/base64Decode/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import base64
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    base64str = req.params.get('base64Decode')
+    if not base64str:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            base64str = req_body.get('base64Decode')
+
+    if base64str:
+        dataDecoded=base64.b64decode(base64str)
+        return func.HttpResponse(f"Output: {dataDecoded}")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
+
+
+
