7b25cf4dec8213933204d0d0aed736729aff6246
Yozelinm <yozelin.m_206@yahoo.com>
2020-01-24 22:42:17 -0800

    update poweshell script
    updating deploy script to create new resources

--- devops/IoT/deploy.ps1
+++ devops/IoT/deploy.ps1
@@ -1,12 +1,12 @@
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+$iotHubName= Read-Host -Prompt "Enter the storage account name"
 
-# Create the storage account.
-$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
-  -Name $storageAccountName `
+# Create the IotHub account.
+$iotHub = New-AziotHubAccount -ResourceGroupName $resourceGroupName `
+  -Name $iotHubName `
   -Location $location `
   -SkuName "Standard_LRS"
 
 # Retrieve the context.
-$ctx = $storageAccount.Context
+$ctx = $iotHub.Context
