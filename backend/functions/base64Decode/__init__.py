588a6f0538b3d5918d8caeef499cc9fe4234770c
hken920 <hken920@gmail.com>
2020-01-26 14:14:50 -0800

    created logs

--- backend/functions/base64Decode/__init__.py
+++ backend/functions/base64Decode/__init__.py
@@ -18,8 +18,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if base64str:
         dataDecode=base64.b64decode(base64str)
+        logging.info('Base64 decode complete. Output: ' + str(dataDecode))
         return func.HttpResponse(f"Output: {dataDecode}!")
     else:
+        logging.warning('Missing input')
         return func.HttpResponse(
              "Please pass a base64str on the query string or in the request body",
              status_code=400
