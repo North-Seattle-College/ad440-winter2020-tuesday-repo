ff6f2bdfe9b6646068ddce12becaeb404597a06f
Yozelinm <yozelin.m_206@yahoo.com>
2020-01-28 00:08:47 -0800

    updating poweshell script
    updating poweshell script

--- devops/IoT/deploy.ps1
+++ devops/IoT/deploy.ps1
@@ -1,12 +1,11 @@
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$iotHubName= Read-Host -Prompt "Enter the storage account name"
+$iotHubName= Read-Host -Prompt "Enter the IoT Hub name"
 
 # Create the IotHub account.
-$iotHub = New-AziotHubAccount -ResourceGroupName $resourceGroupName `
+$iotHubName = New-AzIotHub -ResourceGroupName $resourceGroupName `
   -Name $iotHubName `
   -Location $location `
-  -SkuName "Standard_LRS"
 
 # Retrieve the context.
 $ctx = $iotHub.Context
