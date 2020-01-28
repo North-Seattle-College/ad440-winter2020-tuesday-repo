9ab055ff1cbec633c79d7be0285d05a4eceb1bf9
Yozelinm <yozelin.m_206@yahoo.com>
2020-01-28 14:46:30 -0800

    updating deploy file
    Changed variables and functions. Added new script from Microsoft documentation tutorial and tested on cloud shell

--- devops/IoT/deploy.ps1
+++ devops/IoT/deploy.ps1
@@ -1,11 +1,5 @@
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$iotHubName= Read-Host -Prompt "Enter the IoT Hub name"
 
-# Create the IotHub account.
-$iotHubName = New-AzIotHub -ResourceGroupName $resourceGroupName `
-  -Name $iotHubName `
-  -Location $location `
-
-# Retrieve the context.
-$ctx = $iotHub.Context
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$HOME/template.json"
