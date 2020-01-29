8922b6b4061281d46a1f6c13be07fc50b59de4b9
Yozelinm <yozelin.m_206@yahoo.com>
2020-01-28 17:33:14 -0800

    changed file path in deploy script
    changed file path in deploy script

--- devops/IoT/deploy.ps1
+++ devops/IoT/deploy.ps1
@@ -2,4 +2,4 @@ $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$HOME/template.json"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$yozelinm/ad440-winter2020-tuesday-repo/devops/iot/template.json"
