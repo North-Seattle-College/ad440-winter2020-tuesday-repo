0f110082bde9e36257b3a27ec1c8dc9708ba810b
Abdifatah.206 <shire2567@gmail.com>
2020-01-29 21:51:17 -0800

    login  button with AAD

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './css/App.css';
 import TextComponent from './components/TextComponent';
 import ToggleSwitch from './components/toggleSwitch';
+import  Login from './components/login';
 // renders main page together omponents/TextComponent';with new Text Component
 function App() {
-  
+
   return (
     <div className="App">
-      <header className="App-header"> 
-        <ToggleSwitch/>  
+    <div className="login">
+    < Login />
+    </div>
+      <header className="App-header">
+        <ToggleSwitch/>
         <TextComponent/>
       </header>
     </div>
