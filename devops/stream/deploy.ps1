e4a5f0d750f6a8cd0987f865d86c04cf03f909e1
Thomas <tcoducayen@gmail.com>
2020-02-04 19:40:59 -0800

    added deploy.ps1 file and basic template

---
+++ devops/stream/deploy.ps1
@@ -0,0 +1,70 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+ .DESCRIPTION
+    Deploys an Azure Stream Analytics Template
+ .PARAMETER owner
+    The name of the owner that created the stream.
+ .PARAMETER email
+    The email of the owner that created the stream.
+ .PARAMETER jobName
+    The number of the current job this template reflects
+ .PARAMETER app
+    The name of the App this template is for
+ .PARAMETER storage
+    The storage used for the streaming.
+ .PARAMETER featureName
+    The current feature this stream satisfies
+ .PARAMETER task
+    The current task this stream satisfies
+ .PARAMETER templateFilePath
+    Optional, path to the template file. Defaults to template.json
+ .PARAMETER parametersFilePath
+    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+#>
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]$owner,
+
+ [Parameter(Mandatory=$True)]
+ [string]$email,
+
+ [Parameter(Mandatory=$True)]
+ [string]$jobName = "job1",
+
+ [Parameter(Mandatory=$True)]
+ [string]$app = "Pot-O-Gold",
+
+ [Parameter(Mandatory=$True)]
+ [string]$storage = "kiastrfeatusw2task3",
+
+ [Parameter(Mandatory=$True)]
+ [string]$featureName = "bet-rg-feat-usw2",
+
+ [Parameter(Mandatory=$True)]
+ [string]$task = "task2",
+
+ [Parameter(Mandatory=$True)]
+ [string]$templateFilePath = ".\template.json",
+
+ [Parameter(Mandatory=$True)]
+ [string]$parametersFilePath = ".\parameters.json",
+)
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+
+$owner = Read-Host -Prompt "Enter the Owner name"
+$email = Read-Host -Prompt "Enter the Owner name"
+
+# Create the storage account.
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+  -Name $owner `
+  -Location $location `
+  -SkuName "Standard_LRS"
+
+# Retrieve the context.
+$ctx = $storageAccount.Context
