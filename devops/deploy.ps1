916c1ca2a84a0cff487d5af6c0f2b2fb769c1fb5
iroenu <weguan@hotmail.com>
2020-02-04 19:20:33 -0800

    Update deploy.ps1

--- devops/deploy.ps1
+++ devops/deploy.ps1
@@ -2,6 +2,6 @@ $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 
-$templateUri = "https://github.com/North-Seattle-College/ad440-winter2020-tuesday-repo/blob/feature-azurefunction-wenbin-task1/devops/template.json"
+$templateUri = "https://raw.githubusercontent.com/iroenu/hello-world/master/template.json"
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -Location $location
