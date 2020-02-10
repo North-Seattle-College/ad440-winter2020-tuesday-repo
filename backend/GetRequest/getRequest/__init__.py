71350f92b577c1c8581e5462ebf0272f4783b1fb
Sanford Winslow <sanford.winslow@gmail.com>
2020-02-10 14:14:44 -0800

    adding get request

--- backend/GetRequest/getRequest/__init__.py
+++ backend/GetRequest/getRequest/__init__.py
@@ -1,28 +1,24 @@
 import logging
-import json
 import azure.functions as func
-import requests
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    
-    logging.info(req.__dict__)
-    #logging.info(f"Params: {params}")
-    
+    logging.info("Python HTTP trigger function processed a request.")
    
-    method = req.method
-    if(method == 'GET'):
+    if (req.method == "GET"):
+        
         try:
             req_body = req.get_json()
-            machine= req_body.get("machine")
-            id=req_body.get("id")
-            message=f"Machine: {machine}, ID: {id}"
-            logging.info(req_body)
-            logging.info(f'Python HTTP triggered function processed: {id}')
+            machine= req.params.get("machine")
+            machineId = req.params.get("machineId")
+            message=f"Machine: {machine}, machineId: {machineId}"
+            logging.info(f'Python HTTP triggered function processed machine: {machine}')
+            logging.info(f'Python HTTP triggered function processed machineId: {machineId}')
             return func.HttpResponse(message)
+        
         except ValueError:
-            pass
+           pass
+       
     else:
         logging.info("Not a GET request")
   
