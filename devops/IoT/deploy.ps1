90aea146dfcb1bca27080f1bdb5127a909b15d0b
Yozelinm <yozelin.m_206@yahoo.com>
2020-02-10 22:06:54 -0800

    finalizing template and ps

--- devops/IoT/deploy.ps1
+++ devops/IoT/deploy.ps1
@@ -1,13 +1,10 @@
 #lists all current IoT Hubs resources
 Get-AzIotHub
 
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$templateFile = Read-Host -Prompt "Enter file path to your template"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 
-New-AzIotHub `
--resourceGroupName $resourceGroupName
--Location $location
--TemplateUri $templateFile
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$templateUri = "https://raw.githubusercontent.com/Yozelinm/Yozelinm.github.io/master/iottemplate.json"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -Location $location
 
 
