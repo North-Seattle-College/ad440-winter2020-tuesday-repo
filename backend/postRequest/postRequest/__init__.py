7156ed87eb38a4ea7f4cf62bd76b52b4b96e2967
hken920 <hken920@gmail.com>
2020-02-10 16:00:20 -0800

    added comments

--- backend/postRequest/postRequest/__init__.py
+++ backend/postRequest/postRequest/__init__.py
@@ -6,19 +6,23 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-
+    # check that it is a POST request
     if req.method == "POST":
         try:
+            # check JSON file
             req_body = req.get_json()
             logging.info(req_body)
-            return func.HttpResponse(f"Success")
+            #POST request sucessful
+            return func.HttpResponse(f"Successful request")
         except ValueError:
             pass
     else:
-        logging.info("Not a POST request")
+        # request was not a POST
+        logging.info("Request was not a POST")
+        return func.HttpResponse('Request was not a POST')
 
-
+     # no POST request was made
     return func.HttpResponse(
-             "Please pass a POST request on the query string or in the request body",
+             "Please pass a POST request in the request body",
              status_code=400
         )
