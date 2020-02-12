7de5826f6ec24c70132873fa1254e8fbfbbb402d
Abdifatah.206 <shire2567@gmail.com>
2020-02-11 20:13:41 -0800

    react adal error fixed

---
+++ ui/src/components/Api.js
@@ -0,0 +1,29 @@
+// src/services/Api.js
+import axios from 'axios'
+
+import ApiConfig from './ApiConfig'
+import AdalConfig from './AdalConfig'
+import AuthContext from './Auth'
+
+const instance = axios.create(ApiConfig)
+
+// Add a request interceptor
+instance.interceptors.request.use((config) => {
+  // Check and acquire a token before the request is sent
+  return new Promise((resolve, reject) => {
+    AuthContext.acquireToken(AdalConfig.endpoints.api, (message, token, msg) => {
+      if (!!token) {
+        config.headers.Authorization = `Bearer ${token}`
+        resolve(config)
+      } else {
+        // Do something with error of acquiring the token
+        reject(config)
+      }
+    })
+  })
+}, function(error) {
+  // Do something with error of the request
+  return Promise.reject(error)
+})
+
+export default instance
---
+++ ui/src/components/ApiConfig.js
@@ -0,0 +1,4 @@
+// src/config/ApiConfig.js
+export default {
+  baseURL: "http://localhost:3000" // something like "http://my-host-name.xyz/api"
+}
