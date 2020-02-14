bf5101660472b4507e6eaefab34d6f273b92d70d
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-02-13 22:48:48 -0800

    modifying the function code

--- backend/putRequest/putRequest/__init__.py
+++ backend/putRequest/putRequest/__init__.py
@@ -3,12 +3,9 @@ import azure.functions as func
 
 # main method takes an Http request as parameter.
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    headers = {"my-http-header": "some-value"}
+    
     logging.info('Python HTTP trigger function processed a request.')
     logging.info(req.__dict__)
-
-    #method = req.method
-    machineId = req.params.get('machineId')
 
     # This stament checks for a PUT request.
     if (req.method == "PUT"):
@@ -18,8 +15,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse(f"successful request")
         except ValueError:
             pass
-        else:
-            machineId = req_body.get('machineId')
     # it returns an error Http code that is not a PUT request.
     else:
         logging.info("Not a PUT request...")
@@ -28,12 +23,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             'Request was not a PUT',
             status_code=400
             )
-    # returns the machine id 
-    if machineId:
-        return func.HttpResponse(f"Your machineId is: {machineId}!", headers=headers)
-    else:
-        # retuerns a Http 400 status bad request. 
-        return func.HttpResponse(
-            "Please pass a PUT request on the query string or in the request body",
-            status_code=400
-        )
+    
+    # retuerns a Http 400 status bad request. 
+    return func.HttpResponse(
+        "Please pass a PUT request on the query string or in the request body",
+        status_code=400
+    )
