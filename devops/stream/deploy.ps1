353902e698cb42dfea24e7cb78ac2432504ac512
Thomas <tcoducayen@gmail.com>
2020-02-17 15:39:43 -0800

    updated deployment with functioning parameter uri and template from git

--- devops/stream/deploy.ps1
+++ devops/stream/deploy.ps1
@@ -1,75 +1,27 @@
 <#
- .SYNOPSIS
-    Deploys a template to Azure
- .DESCRIPTION
-    Deploys an Azure Stream Analytics Template
- .PARAMETER owner
-    The name of the owner that created the stream.
- .PARAMETER email
-    The email of the owner that created the stream.
- .PARAMETER jobName
-    The number of the current job this template reflects
- .PARAMETER app
-    The name of the App this template is for
- .PARAMETER storage
-    The storage used for the streaming.
- .PARAMETER resourceGroupName
-    The current feature this stream satisfies
- .PARAMETER task
-    The current task this stream satisfies
- .PARAMETER templateFilePath
-    Optional, path to the template file. Defaults to template.json
- .PARAMETER parametersFilePath
-    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+  This deployment uses powershell in order to create a stream analytics job
+
+  Here is what is created:
+  ### IotHubs
+  ### Blob storage
+  ### Azure streaming analytics job
+
+  You need all of these elements in order to suceessfully launch a stream analytics resource
 #>
 
-#Queries all of the entries necessary to deploy the Stream Job
-param(
- [Parameter(Mandatory=$True)]
- [string]$owner,
+param (
+# Name of the current user
+[string] [Parameter(Mandatory=$true)] $owner,
 
- [Parameter(Mandatory=$True)]
- [string]$email,
+# Name of the current user email
+[string] [Parameter(Mandatory=$true)] $email,
 
- [Parameter(Mandatory=$True)]
- [string]$jobName = "job1",
-
- [Parameter(Mandatory=$True)]
- [string]$app = "Pot-O-Gold",
-
- [Parameter(Mandatory=$True)]
- [string]$storage = "kiastrfeatusw2task3",
-
- [Parameter(Mandatory=$True)]
- [string]$resourceGroupName = "bet-rg-feat-usw2",
-
- [Parameter(Mandatory=$True)]
- [string]$task = "task2",
-
- [Parameter(Mandatory=$True)]
- [string]$templateFilePath = ".\template.json",
-
- [Parameter(Mandatory=$True)]
- [string]$parametersFilePath = ".\parameters.json"
-
- [Parameter(Mandatory=$True)]
- [string]$parametersFilePath = ".\parameters.json"
+# Name of the resource group being created
+[string] [Parameter(Mandatory=$true)] $resourceGroupName,
 )
 
-#******************************************************************************
-# Script body
-# Execution begins here
-#******************************************************************************
-
-$owner = Read-Host -Prompt "Enter owner name: "
-$email = Read-Host -Prompt "Enter email: "
-# $jobname = Read-Host -Prompt "Enter job name: "
-# $location = ""
-# $resourceGroupName = Read-Host -Prompt "Enter resource group name: "
-
-# Create the streaming job
-New-AzStreamAnalyticsJob -ResourceGroupName $resourceGroupName `
-  -ResourceGroupName $resourceGroupName `
-  -File $jobDefinitionFile `
-  -Name $jobName `
-  -Force
+#Test-AzResourceGroupDeployment ` #use this line to test and comment out line below
+New-AzResourceGroupDeployment `
+  -Name $owner -ResourceGroupName $resourceGroupName `
+  -TemplateUri https://raw.githubusercontent.com/North-Seattle-College/ad440-winter2020-tuesday-repo/feature-thomas-3/devops/stream/template.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/North-Seattle-College/ad440-winter2020-tuesday-repo/feature-thomas-3/devops/stream/parameters.json `
