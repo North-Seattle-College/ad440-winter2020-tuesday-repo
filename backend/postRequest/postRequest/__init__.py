a2564194fa2e3405406fa1ae6bf6d5c07a256036
hken920 <hken920@gmail.com>
2020-02-22 18:10:00 -0800

    added os package

--- backend/postRequest/postRequest/__init__.py
+++ backend/postRequest/postRequest/__init__.py
@@ -1,28 +1,27 @@
 import logging
 import pyodbc
 import azure.functions as func
+import os
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
+    # connect to database
+    cnxn = pyodbc.connect(os.environ['DatabaseConnString'])
+    logging.info('Connect to database complete')
+    cursor = cnxn.cursor()
+
     # check that it is a POST request
-    # if req.method == "POST":
     try:
         # check JSON file
         req_body = req.get_json()
         logging.info(req_body)
 
-        #connect to database
-        cnxn = pyodbc.connect(#connection name
-        )
-        logging.info('Connect to database complete')
-
-        cursor = cnxn.cursor()
-        #insert data into SQL
+        # insert data into SQL
         cursor.execute(
-            "INSERT INTO Machines (Model, ModelNum, ModelPhoto, SerialNum, VendorID, LocationID)\
-                VALUES (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'],req_body['VendorID'],req_body['LocationID'])"
+            "INSERT INTO [dbo].[Machines] ([Model], [ModelNum], [ModelPhoto], [SerialNum], [VendorID], [LocationID])\
+                VALUES (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'])"
             )
 
         logging.info("Data inserted into SQL complete.")
@@ -30,13 +29,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Successful request")
     except ValueError:
         pass
-    # else:
-    #     # request was not a POST
-    #     logging.info("Request was not a POST")
-    #     return func.HttpResponse(
-    #         'Request was not a POST',
-    #         status_code=400
-    #         )
 
      # no request was made
     logging.info("No POST request was made")
