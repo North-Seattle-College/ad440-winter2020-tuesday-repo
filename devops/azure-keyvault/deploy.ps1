dec023610af8f1b0524df8d70eb108d9bf719839
Thomas <tcoducayen@gmail.com>
2020-02-24 11:39:24 -0800

    removed all testing constraints

--- devops/azure-keyvault/deploy.ps1
+++ devops/azure-keyvault/deploy.ps1
@@ -1,32 +1,32 @@
 <#
-  This is the deploy template for creating key vaults
+  Creates an empty key vaults
 #>
 
-<#
-Will undo comment once this launches correctly
 
 #get variables from the admin who is creating
 $projectName = Read-Host -Prompt "Enter acronym for the project name being (i.e. pog for Pot-o-Gold)"
 $location = Read-Host -Prompt "Enter the same location that is used for creating the key vault (i.e. westus2)"
+
+<#
+#only used for testing purposes, uncomment if testing:
+$projectName = "pog" #refers to pot of gold
+$location = "westus2" #WA location
 #>
 
-#delete after testing:
-$projectName = "pog"
-
 #create variables
-$resourceGroupName = "test-${projectName}-vault"
-$location = "westus2"
+$resourceGroupName = "${projectName}-vault"
 
 #add templates for the file and parameters
 $template = ".\template.json"
 $parameters = ".\parameters.json"
 
+#created resource group only if it does not exist
 Get-AzResourceGroup -Name $resourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
 if ($notPresent) {
-  #create new resource group
   New-AzResourceGroup -Name $resourceGroupName -Location $location
 }
 
+#Creates empty key vault
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -TemplateFile $template `
