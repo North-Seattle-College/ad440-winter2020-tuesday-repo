f067dbfe0693ee869e54d504b21a1278a0b47689
Thomas <tcoducayen@gmail.com>
2020-02-24 11:02:36 -0800

    created deploy script

--- devops/azure-keyvault/deploy.ps1
+++ devops/azure-keyvault/deploy.ps1
@@ -1,3 +1,15 @@
 <#
-  This is the beginning of the deploy script...
+  This is the deploy template for creating key vaults
 #>
+
+#get variables from the admin who is creating
+$projectName = Read-Host -Prompt "Enter acronym for the project name being (i.e. pog for Pot-o-Gold)"
+$location = Read-Host -Prompt "Enter the same location that is used for creating the key vault (i.e. westus2)"
+
+#create variables
+$resourceGroupName = "${projectName}-keyvault"
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile "./template.json" `
+    -TemplateParameterFile "./parameters.json"
