61b3f44be1711c48771a4469f8a0db79c6a9432f
Thomas <tcoducayen@gmail.com>
2020-02-24 13:06:19 -0800

    prepared for test

--- devops/azure-keyvault/deploy.ps1
+++ devops/azure-keyvault/deploy.ps1
@@ -1,10 +1,15 @@
 <#
-  Creates an empty key vaults
+  Creates an empty key vault.
+
+  Key vaults must be unique and after deleting a key vault, it will be "soft deleted"
+  for 90 days before it is premanently gone.  Only the subscription owner can purge the
+  key vault and as such, it will be unable to be copied once deleted.  If testing, try
+  using a different resource group and manually changing the parameters.json for each test.
 #>
 
 
 #get variables from the admin who is creating
-$resourceGroupName = Read-Host -Prompt "Enter resource group name (i.e. nam-rg-usw2-task128): "
+$resourceGroupName = Read-Host -Prompt "Enter resource group name (i.e. {abbr_name}-rg-usw2-task#): "
 $location = Read-Host -Prompt "Enter the same location that is used for creating the key vault (i.e. westus2): "
 
 <#
@@ -28,8 +33,3 @@ New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -TemplateFile $template `
     -TemplateParameterFile $parameters
-
-<#
-#removes resource group if needed
-Remove-AzResourceGroup -Name $resourceGroupName
-#>
