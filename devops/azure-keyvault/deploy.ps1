18e501d87aefbbda0380349e86d5316167be3b17
Thomas <tcoducayen@gmail.com>
2020-02-24 11:07:45 -0800

    prepare deploy for testing

--- devops/azure-keyvault/deploy.ps1
+++ devops/azure-keyvault/deploy.ps1
@@ -2,14 +2,21 @@
   This is the deploy template for creating key vaults
 #>
 
+<#
+Will undo comment once this launches correctly
+
 #get variables from the admin who is creating
 $projectName = Read-Host -Prompt "Enter acronym for the project name being (i.e. pog for Pot-o-Gold)"
 $location = Read-Host -Prompt "Enter the same location that is used for creating the key vault (i.e. westus2)"
+#>
+
+#delete after testing:
+$projectName = "pog"
 
 #create variables
 $resourceGroupName = "${projectName}-keyvault"
 
-New-AzResourceGroupDeployment `
+Test-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -TemplateFile "./template.json" `
     -TemplateParameterFile "./parameters.json"
