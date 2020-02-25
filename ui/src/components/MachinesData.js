9c7efe93a778428e3cd398164d259d82b322c250
siergieyk <siergieyk@gmail.com>
2020-02-24 18:37:30 -0800

    feature-siergiey-task#148
    Created the ADD, REMOVE, and EDIT functions.

---
+++ ui/src/components/MachinesData.js
@@ -0,0 +1,11 @@
+
+export const MachineData = 
+[
+  { "id": 1, "vendor": "Microsoft", "address": "5000 148th Ave NE, Redmond, WA 98052", "status": "broken"},
+  { "id": 2, "vendor": "North Seattle College", "address": "9600 College Way N, WA 98103", "status": "possible motor wear"},
+  { "id": 3, "vendor": "Central Seattle College", "address": "539 12th Ave, Redmond, WA 98052", "status": "working"},
+  { "id": 4, "vendor": "Some Fun Office", "address": "50 Occidental Ave S, WA 98124", "status": "broken water pipe"},
+  { "id": 5, "vendor": "Fidelity", "address": "4600 5th Ave, Seattle, WA 980104", "status": "coffee jam"}
+  
+
+]
--- ui/src/components/PageTabs.js
+++ ui/src/components/PageTabs.js
@@ -10,6 +10,7 @@ import {
   TabPanel as BaseTabPanel
 } from "reakit/Tab";
 import '../css/PageTabs.css';
+import MachinesMain from './MachinesMain';
 
 const TabsContext = React.createContext();
 
@@ -58,7 +59,8 @@ export default function PageTabs() {
         <Tab stopId="tab11">Tab 11</Tab>
         <Tab stopId="tab12">Tab 12</Tab>
 
-<TabPanel stopId="tab1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+<TabPanel stopId="tab1">
+      <MachinesMain/>
       </TabPanel>
 
 <TabPanel stopId="tab2">Where does it come from?
