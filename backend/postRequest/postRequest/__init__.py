e67173585396a6151784f30d443f584172b18d68
hken920 <hken920@gmail.com>
2020-02-28 15:10:45 -0800

    complete connection and data input.  connection string hardcoded

--- backend/postRequest/postRequest/__init__.py
+++ backend/postRequest/postRequest/__init__.py
@@ -9,34 +9,49 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     
     # connect to database
     try:
-        cnxn = pyodbc.connect(os.environ['odbc13ConnStting'])
-        logging.info('Connect to database complete')    
-        # cursor = cnxn.cursor()
-        return func.HttpRequest(f"Successful database connection")
+        conn = pyodbc.connect( 
+            "Driver={ODBC Driver 17 for SQL Server};" 
+            "Server=tcp:yoz-sql-server-task2.database.windows.net,1433;" 
+            "Database=yoz-sql-feat-usw2-task2;Uid=devops-admin-t2;" 
+            #input password here
+            "Pwd=---PASSWORD HERE----;" 
+            "Encrypt=yes;" 
+            "TrustServerCertificate=no;" 
+            "Connection Timeout=30;" 
+            )
+        cursor = conn.cursor()
+        logging.info('Connect to database complete')  
     except:
+        #connection failed
         logging.exception("Database connection failed")
 
-    # check that it is a POST request
     try:
         # check JSON file
         req_body = req.get_json()
         logging.info(req_body)
-
-        # insert data into SQL
-        # cursor.execute(
-        #     "INSERT INTO [dbo].[Machines] ([Model], [ModelNum], [ModelPhoto], [SerialNum], [VendorID], [LocationID])\
-        #         VALUES (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'])"
-        #     )
-        # logging.info("Data inserted into SQL complete.")
         
-        #POST request successfulS
-        return func.HttpResponse(f"Successful request")
+        #check for duplicate Models
+        cursor.execute('SELECT COUNT (*) FROM [dbo].[Machines] WHERE Model= ?', (req_body['Model']))
+        result = cursor.fetchone()
+        found = result[0]
+        if found == 0:                 
+            # no duplicate Model was found
+            # insert data into SQL
+            cursor.execute(
+                '''INSERT INTO [dbo].[Machines] (Model, ModelNum, ModelPhoto, SerialNum, VendorID, LocationID)
+                VALUES (?,?,?,?,?,?)''', (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'])
+                )
+            conn.commit()
+            logging.info("Data inserted into SQL complete.")
+        
+        #POST request successful
+        return func.HttpResponse(f"Successful request. Data inserted into database")
     except ValueError:
         pass
 
-     # no request was made
+     # no POST request was made
     logging.info("No POST request was made")
-    return func.HttpResponse(
-        "Please pass a POST request in the request body",
+    return func.HttpResponse(   
+        "Please pass a POST request in the request body",   
         status_code=400 
         )
