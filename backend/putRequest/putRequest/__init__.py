41cefa236618ae0de2b6b973d647c9a7ffc8dd38
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-02-29 13:13:27 -0800

    Update __init__.py
    successful logic to update machines

--- backend/putRequest/putRequest/__init__.py
+++ backend/putRequest/putRequest/__init__.py
@@ -7,24 +7,37 @@ import pyodbc
 # main method takes an Http request as parameter.
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(req.__dict__)
+
     try:
         # Setting connection to the database via key vault connection string.
-        #conn = pyodbc.connect( "Driver={ODBC Driver 11 for SQL Server};" "Server=tcp:yoz-sql-server-task2.database.windows.net,1433;" "Database=yoz-sql-feat-usw2-task2;Uid=devops-admin-t2;" "Pwd=Sparky2018!;" "Encrypt=yes;" "TrustServerCertificate=no;" "Connection Timeout=30;" )
-        cnxn = pyodbc.connect(os.environ["ConnStringDB"])
-        cursor = cnxn.cursor()
-        
-        logging.info("Connecting to the database...")
-        return HttpResponse("Connection complete")
-    except:
-        logging.info("Connection to the database failed")
+        conn = pyodbc.connect(os.environ['ConnString'])
+        logging.info("Connection complete")
+        cursor = conn.cursor()
+    except Exception as e:
+        logging.error("Connection to db failed" + str(e))
+        print(e)
+        pass
+    
     # This stament checks for a PUT request.
     if (req.method == "PUT"):
         try:
+            # check JSON body request
             req_body = req.get_json()
             logging.info(req_body)
-            
+            print(req_body)
+
+            # updates a Machine 
+            cursor.execute(''' UPDATE [dbo].[Machines] 
+            SET Model = ?, ModelNum = ?, ModelPhoto = ?, SerialNum = ?, VendorID = ?, LocationID = ?  WHERE MachineID = ? ''', 
+            (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'], req_body['MachineID'])
+            )               
+            conn.commit()
+            logging.info("record successfuly updated") 
+                
             return func.HttpResponse(f"successful request")
-        except ValueError:
+        except Exception as e:
+            logging.error("Connecting to the database failed" + str(e))
+            print(str(e))
             pass
     # it returns an error Http code that is not a PUT request.
     else:
