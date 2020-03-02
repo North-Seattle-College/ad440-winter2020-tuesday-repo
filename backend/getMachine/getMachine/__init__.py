9882fc120799b8cb4d67847283348c277b307962
eatlunchnow <eatlunchnow@gmail.com>
2020-03-01 16:20:02 -0800

    Update __init__.py

--- backend/getMachine/getMachine/__init__.py
+++ backend/getMachine/getMachine/__init__.py
@@ -1,45 +1,48 @@
 import logging
 import azure.functions as func
-#import pyodbc
-#import os 
+import pyodbc
+import os
+import json
 
-#HttpRequest parameter 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    #cnxn = pyodbc.connect(os.environ['DatabaseConnString'])
-    #cnxn = pyodbc.connect()
 
-   # try:
-        #req_body = req.get_json()
-        #logging.info(req_body)
-        #cursor = cnxn.cursor()
-       #cursor.execute("SELECT * FROM [dbo].[Machines]")
+    try:
+        #database connection 
+        conct = pyodbc.connect(
+             "Driver={ODBC Driver 17 for SQL Server};" 
+            "Server=;"
+            "Database=;" 
+            "Pwd=;" "Encrypt=yes;" 
+            "TrustServerCertificate=no;" 
+            "Connection Timeout=30;"
+            )
+        cursor = conct.cursor()
+        #query to select everything from the Machines table and put results in a tuple then convert tuples into dictionary
+        rows = cursor.execute('''SELECT * FROM [dbo].[Machines]''').fetchall()
+        column_names = [column[0] for column in cursor.description]
+        rows = [dict(zip(column_names, row)) for row in rows]
 
-        #return func.HttpResponse(f"Successful")
-    #except ValueError:
-        #pass
+        conct.commit()
 
-    #logging.info("Not a GET request")
-    #return func.HttpResponse("Please pass GET request in body",
-    #status_code=400)
+        #json.dumps to create JSON string
+        return func.HttpResponse(json.dumps(rows))
+    except:
+        logging.exception("Database connection failed")    
 
-
-#get request to find machines table
-    name = req.params.get('machines')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('machines')
-
-  #returns all data in machines table
-  #if name is not returned shows error
-    if name:
-        return func.HttpResponse(f"{name}")
-    else:
-        return func.HttpResponse(
-            "Please pass a name on the query string or in the request body",
-            status_code=400
-        )
+    
+    try:
+       
+        req_body = req.get_json()
+        logging.info(req_body) 
+        #if request is successful this success message will show
+        return func.HttpResponse(f"Successful request")
+    except ValueError:
+        pass
+    #if request is not successful this error will show
+    logging.info("No GET request was made")
+    return func.HttpResponse(
+        "Please pass a name on the query string or in the request body",
+        status_code=400
+        )       
