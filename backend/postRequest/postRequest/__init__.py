df9fa96d6984251f5cd321c8d953f2e360b251c9
hken920 <hken920@gmail.com>
2020-03-03 12:45:23 -0800

    added comments, try/except, and logs

--- backend/postRequest/postRequest/__init__.py
+++ backend/postRequest/postRequest/__init__.py
@@ -7,49 +7,59 @@ import os
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    # connect to database
     try:
-        logging.info('Starting connectiong to database...')
+        # connect to database
+        logging.info('Starting connection to database...')
         conn = pyodbc.connect(os.environ['ConnString'])
         cursor = conn.cursor()
-        logging.info('Connected to database complete')  
+        logging.info('Connected to database')  
     except:
-        #connection failed
+        # connection failed
         logging.exception("Database connection failed")
 
     try:
-        # check JSON file
+        # read JSON body
         req_body = req.get_json()
         logging.info('Content of JSON body: ' + str(req_body))
         
-        #check for duplicate MachineID
-        logging.info('Checking for duplicate machine...')
-        cursor.execute('SELECT COUNT (*) FROM [dbo].[Machines] WHERE MachineID= ?', (req_body['MachineID']))
-        result = cursor.fetchone()
-        found = result[0]
+        try:
+            # check for duplicate MachineID
+            logging.info('Checking for duplicate machine...')
+            cursor.execute('SELECT COUNT (*) FROM [dbo].[Machines] WHERE MachineID= ?', (req_body['MachineID']))
+            result = cursor.fetchone()
+            found = result[0]
+        except:
+            #Duplicate check failed
+            logging.exception('Duplicate check failed')
+
         if found == 0:                 
             # no duplicate MachineID was found
-            # insert data into SQL
-            logging.info('No duplicate found.\n Inserting new Machine...')
+            logging.info('No duplicate found. \nInserting new Machine...')
+
             try:
+                # insert data into SQL
                 cursor.execute(
                     '''INSERT INTO [dbo].[Machines] (Model, ModelNum, ModelPhoto, SerialNum, VendorID, LocationID)
                     VALUES (?,?,?,?,?,?)''', (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'])
                     )
             except:
+                # data insert failed
                 logging.exception('Failed to insert new Machine')
 
             try:
+                # commit data to database
                 conn.commit()
             except:
+                # data commit failed
                 logging.exception('Failed to commit new machine')
-
-            logging.info("Data inserted into SQL complete.")
+                
         else:
-            logging.info('Data already exists in database')
+            # duplicate found in database
+            logging.info('Machine already exists in database')
 
-        #POST request successful
-        return func.HttpResponse(f"Successful request.")
+        # POST request successful
+        logging.info('Http trigger request complete')
+        return func.HttpResponse(f"Successful request")
     except ValueError:
         pass
 
@@ -59,6 +69,3 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         "Please pass a POST request in the request body",   
         status_code=400 
         )
-
-
-
