87b51d60490ac60d6c630bea4d33244cb20de69f
hken920 <hken920@gmail.com>
2020-03-03 11:39:31 -0800

    added try/except. added info and exception logs

--- backend/postRequest/postRequest/__init__.py
+++ backend/postRequest/postRequest/__init__.py
@@ -9,9 +9,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     
     # connect to database
     try:
+        logging.info('Starting connectiong to database...')
         conn = pyodbc.connect(os.environ['ConnString'])
         cursor = conn.cursor()
-        logging.info('Connect to database complete')  
+        logging.info('Connected to database complete')  
     except:
         #connection failed
         logging.exception("Database connection failed")
@@ -19,24 +20,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         # check JSON file
         req_body = req.get_json()
-        logging.info(req_body)
+        logging.info('Content of JSON body: ' + str(req_body))
         
         #check for duplicate MachineID
+        logging.info('Checking for duplicate machine...')
         cursor.execute('SELECT COUNT (*) FROM [dbo].[Machines] WHERE MachineID= ?', (req_body['MachineID']))
         result = cursor.fetchone()
         found = result[0]
         if found == 0:                 
             # no duplicate MachineID was found
             # insert data into SQL
-            cursor.execute(
-                '''INSERT INTO [dbo].[Machines] (Model, ModelNum, ModelPhoto, SerialNum, VendorID, LocationID)
-                VALUES (?,?,?,?,?,?)''', (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'])
-                )
-            conn.commit()
+            logging.info('No duplicate found.\n Inserting new Machine...')
+            try:
+                cursor.execute(
+                    '''INSERT INTO [dbo].[Machines] (Model, ModelNum, ModelPhoto, SerialNum, VendorID, LocationID)
+                    VALUES (?,?,?,?,?,?)''', (req_body['Model'], req_body['ModelNum'], req_body['ModelPhoto'], req_body['SerialNum'], req_body['VendorID'], req_body['LocationID'])
+                    )
+            except:
+                logging.exception('Failed to insert new Machine')
+
+            try:
+                conn.commit()
+            except:
+                logging.exception('Failed to commit new machine')
+
             logging.info("Data inserted into SQL complete.")
-        
+        else:
+            logging.info('Data already exists in database')
+
         #POST request successful
-        return func.HttpResponse(f"Successful request. Data inserted into database")
+        return func.HttpResponse(f"Successful request.")
     except ValueError:
         pass
 
