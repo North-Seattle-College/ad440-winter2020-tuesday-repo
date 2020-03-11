280ba7402f1b1e529210fb7b572acf7e4e13734d
Sanford Winslow <sanford.winslow@gmail.com>
2020-03-10 20:27:38 -0700

    machine by id

--- backend/getMachineId/machineId/__init__.py
+++ backend/getMachineId/machineId/__init__.py
@@ -13,27 +13,33 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.info("Connection complete")
         cursor = conn.cursor()
     
-        req_body = req.get_json()
+        #variable for machine id parameter 
         machineId = req.route_params.get('MachineID')
-        logging.info(req_body)
+        logging.info(machineId)
     
         #select specific machine by ID from machine table in database
-        rows = cursor.execute('''SELECT * FROM [dbo].[Machines] WHERE MachineID = ? ''',
-        (machineId['MachineID'])).fetchall()
+        rows = cursor.execute('''SELECT * FROM [dbo].[Machines] WHERE MachineID = ?''', machineId).fetchone()
+        logging.info(rows)
+        
+        #get column names and turn into json file 
         column_names = [column[0] for column in cursor.description]
-        rows = [dict(zip(column_names, row)) for row in rows]
-            
+        result = [dict(zip(column_names, rows))]
+        
+        
+        #commit the query 
         conn.commit()
+        
+        #return the query 
+        return func.HttpResponse(json.dumps(result))
+    
+        #disconnect database
         conn.close()
-        return func.HttpResponse(json.dumps(rows))
-        #return func.HttpResponse(f’successful request’)
+        
     except Exception as e:
         #connection failed
         logging.exception("Connection failed" + str(e))
         pass
-    #except ValueError:
-        #pass
-    #else:
+    
     logging.info('Not a GET request')
     #return func.HttpResponse(
          #“Please pass a GET request”,
