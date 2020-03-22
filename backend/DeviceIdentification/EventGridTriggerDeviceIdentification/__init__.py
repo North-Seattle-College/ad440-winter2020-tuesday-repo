c7a10ac9f7b8ee8ddd28de5e439c6f7f8267ad8d
Laila Chavez <lailamay.chavez@gmail.com>
2020-03-21 22:07:28 -0700

    modified Event Grid Python file

--- backend/DeviceIdentification/EventGridTriggerDeviceIdentification/__init__.py
+++ backend/DeviceIdentification/EventGridTriggerDeviceIdentification/__init__.py
@@ -1,12 +1,13 @@
 # Laila Chavez
 # Event Grid code
 import json
+import os
 import logging
 import requests
 import azure.functions as func 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
-
+logging.info('laila test 319')
 def main(event: func.EventGridEvent):
     #Trigger function with info coming in from simulated IoT device
     result = json.dumps({
@@ -18,22 +19,32 @@ def main(event: func.EventGridEvent):
         'event_test' : 'test string'
             
     }) 
+    test= json.loads(result)
+    test2= test['data']['body']['MachineID']
+    test3=test['data']['body']
+    logging.info(test2)
+
     #Decide what function (GET/PUT/POST) the info goes to
-    url = 'https://san-fun-usw2-task156.azurewebsites.net/api/machines/{MachineID}?code=0QZ7re1VjAXWcYWew1lSfKQWIqEHoE4nVMjoQNyXiajBryooiiz3ZQ=='
-    if requests.get(url) != 404: 
+   # url = f'https://san-fun-usw2-task156.azurewebsites.net/api/machines/{test2}?code=0QZ7re1VjAXWcYWew1lSfKQWIqEHoE4nVMjoQNyXiajBryooiiz3ZQ=='
+    getURL = os.environ["GetEndpoint"]
+
+    if requests.get(getURL) == 200 or 202: 
         #If URL doesn't give back a 404, make a GET request
-        response = requests.get(url)
+        response = requests.get(getURL)
         logging.info(response.text)
         
-
+    #url = requests.put('https://jos-rg-fun-usw2-task62.azurewebsites.net/api/putRequest', json=test2)
+        putURL = os.environ["PutEndpoint"]
         #If URL gives a 500 response, make an update/PUT request
-        if response == 500: 
-            response= requests.put('https://jos-rg-fun-usw2-task62.azurewebsites.net/api/putRequest', json= result)
+        if response == 200 or 202: 
+            logging.info(test3)
+            response= requests.put(putURL, json= test3)
             logging.info(response.text)
         logging.debug("Got response 404.")
     #Otherwise, create a message with a POST request
     else: 
-        response = requests.post('https://ken-fun-feat-usw2-task60.azurewebsites.net', json= result)
+        postURL = os.environ["PostEndpoint"]
+        response = requests.post(postURL, json= test3)
         logging.info(response.text)
         
 
