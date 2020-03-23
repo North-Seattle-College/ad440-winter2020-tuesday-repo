c75929af93c30bcfeef3d63064e7d4bb978a8bcd
Jeremy Westfall <jeremydwestfall@gmail.com>
2020-03-22 20:00:37 -0700

    commented out unused code to remove errors

--- ui/src/components/MachinesSaveNewForm.js
+++ ui/src/components/MachinesSaveNewForm.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
-import { Input, NumericTextBox } from '@progress/kendo-react-inputs';
+// import { Input, NumericTextBox } from '@progress/kendo-react-inputs';
+import { Input } from '@progress/kendo-react-inputs';
 
 export default class MachinesSaveNewForm extends React.Component {
   constructor(props) {
@@ -27,11 +28,11 @@ export default class MachinesSaveNewForm extends React.Component {
       console.log("Edited adress ", editedProperty.address, "Edited name ", editedProperty.name, "Edited status ", editedProperty.status);
       console.log("Product in Edit = this.dataItem ", this.state);
 
-       
-        
+
+
      editedProperty[propertyName] = value;
-        
-        
+
+
     // console.log("editedProperty[propertyName]: ", editedProperty[propertyName])
 
       console.log("Edited[name] ," , editedProperty[propertyName])
@@ -42,7 +43,7 @@ export default class MachinesSaveNewForm extends React.Component {
   }
 
   render() {
-    
+
       return (
         <Dialog
             onClose={this.props.cancel}
@@ -118,7 +119,7 @@ export default class MachinesSaveNewForm extends React.Component {
                         onChange={this.onDialogInputChange}
                     />
                     </label></div>
-                  
+
             </form>
             <DialogActionsBar>
                 <button
