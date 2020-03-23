483b75c6dc71f24e0a7ae7c263034cd7e478b3d2
Thomas <tcoducayen@gmail.com>
2020-03-22 18:47:32 -0700

    undo changes to easier merge

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import TextComponent from './components/TextComponent';
+import TextComponent from './TextComponent';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  const textElement = getByText(/OFF/i);
-  expect(textElement).toBeInTheDocument();
-  expect(linkElement).toBeInTheDocument();
-});
