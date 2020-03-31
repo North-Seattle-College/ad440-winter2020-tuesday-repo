8b24ff39e8d8c2f92cb9617d263a83ff4d443677
Iryna <iryna.sherepot@gmail.com>
2020-03-30 21:45:07 -0700

    initial commit

--- ui/src/components/MachinesButtons.js
+++ ui/src/components/MachinesButtons.js
@@ -9,25 +9,28 @@ export default function MachinesButtons(openEditForm, deletemachine, details) {
     return class extends GridCell {
         render() {
             return (
-                <td>
-                    <button
-                        className="k-primary k-button k-grid-edit-command"
+                <td 
+                //  style = {{width: "14.28571420%" }}
+                >
+                    <button 
+                        id = "edit-button" className="k-primary k-button k-grid-edit-command"
                         onClick={() => {
                             openEditForm(this.props.dataItem.MachineID, this.props.dataItem)
                              console.log("openEditForm(this.props.dataItem.id): ", this.props.dataItem.MachineID)}
                                        }>Edit</button>
 
-                                        &nbsp;
+                                       
+                                        
 
-                      <button
-                                            className="k-primary k-button k-grid-edit-command"
+                      <button 
+                                          id = "details-button" className="k-primary k-button k-grid-edit-command"
                                             onClick={()=> deletemachine(this.props.dataItem.MachineID)}>Remove</button>
 
 
-&nbsp;
+                 
 
-                    <button
-                        className="k-primary k-button k-grid-details-command"
+                    <button 
+                        id = "remove-button" className="k-primary k-button k-grid-details-command"
                         onClick={() => { details(this.props.dataItem);
                                        }}>Details</button>
 
